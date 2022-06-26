fc413d02d8e4c46a1a23931f5a0fe1076c6a443b
Miseon <soup7912@gmail.com>
2022-06-26 16:19:35 +0900

    아마도 마지막

--- main.py
+++ main.py
@@ -30,10 +30,13 @@ scene = cv2.imread('640x480-image.jpg')
 scene = cv2.resize(scene, (640, 480))
 
 
-# 프레임 복사본 생성 후, salt pepper noise를 제거한다.
-cloneImage = image.copy()  # 현재 프레임의 복사본 생성
+# 이미지 복사본 생성 후, salt pepper noise를 제거한다.
+cloneImage = image.copy()  # 현재 이미지의 복사본 생성
 grayImage = cv2.cvtColor(cloneImage, cv2.COLOR_BGR2GRAY)  # 복사본을 회색조 영상으로 변환
-grayImage = cv2.medianBlur(grayImage, 9)  # 블러링으로 노이즈 제거 -> 추후 수정 필요
+grayImage = cv2.medianBlur(grayImage, 9)  # 블러링으로 노이즈 제거
+
+# 노이즈가 제거된 이미지 출력
+cv2.imshow("image Result", image)
 
 # 커널 생성
 kernel = np.ones((2, 2))
@@ -58,18 +61,21 @@ edges = cv2.dilate(edges, kernel, iterations=5)
 edges = cv2.erode(edges, kernel, iterations=2)
 
 # edges 임계치 적용 이미지 출력
-cv2.imshow("Canny result", edges)
+cv2.imshow("Canny", edges)
 
+# image 외곽선 정보 저장
 (cnts, _) = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
 dm = np.zeros_like(edges)
 if len(cnts) > 0:
+    # 배경 : 0, 전경 : 255 => 배경과 전경이 분리된 mask image를 그림
     mcnt = max(cnts[:], key=cv2.contourArea)
     dm = cv2.fillConvexPoly(dm, mcnt, (255))
     cv2.imshow("DM", dm)
 c = image.copy()
-c[dm != 255] = scene[dm != 255]
-cv2.imshow("Canny Result", c)
+# 배경 : 0, 전경 : 255
+c[dm != 255] = scene[dm != 255] # 전경이 아닌 경우에만 배경 이미지의 화소를 대입함
+cv2.imshow("Result", c)
 
 cv2.waitKey(0)
 
